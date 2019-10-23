d9269bd0062d6f31a8e754b9fe5637bed14a2dd9
andrewwags <andrewwags@gmail.com>
2019-10-22 22:52:26 -0700

    Added click to drag camera

--- Assets/Scripts/Core.cs
+++ Assets/Scripts/Core.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Core : MonoBehaviour
 {
-    [SerializeField] Transform target;
+    [SerializeField] Transform mouseTarget;
+    private Vector2 mousePosition;
     private Vector2 clickPosition;
+
+    private Vector3 MouseStart;
+    private float dist;
 
     private float cameraFOV;
     public float minFOV = 5.0f;
@@ -16,8 +20,11 @@ public class Core : MonoBehaviour
 
     void Start()
     {
+        mousePosition = transform.position;
         clickPosition = transform.position;
         cameraFOV = Camera.main.orthographicSize;
+
+        dist = transform.position.z;
     }
 
     void Update()
@@ -28,23 +35,30 @@ public class Core : MonoBehaviour
     //Mouse click player motion control
     void EyeControl()
     {
-        //Move Player position (X and Y axis) to position of mouse click
-        if (Input.GetMouseButtonDown(0))
+        //move mouseTarget game object to match mouse position on screen
+        mousePosition = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseTarget.position = mousePosition;
+
+        //Click and drag mouse to move camera position
+        if (Input.GetMouseButtonDown(2))
         {
-            Debug.Log("Left Mouse Button Pressed");
-            clickPosition = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            target.position = clickPosition;
-            //this should be refactored so that it has to hit the colider of the environment, limiting the click movement only environment
+            MouseStart = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
+            MouseStart = Camera.main.ScreenToWorldPoint(MouseStart);
+            MouseStart.z = transform.position.z;
 
         }
-        if ((Vector2)transform.position != clickPosition)
+        else if (Input.GetMouseButton(2))
         {
-            transform.position = Vector2.MoveTowards(transform.position, clickPosition, moveSpeed * Time.deltaTime);
+            Vector3 MouseMove = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
+            MouseMove = Camera.main.ScreenToWorldPoint(MouseMove);
+            MouseMove.z = transform.position.z;
+            Camera.main.transform.position = transform.position - (MouseMove - MouseStart);
+            
         }
 
         //Zoom in and out player position with scroll wheel
         //scroll wheel controls size of orthographic camera
-        cameraFOV += Input.mouseScrollDelta.y * zoomSensitivity;
+        cameraFOV -= Input.mouseScrollDelta.y * zoomSensitivity;
         cameraFOV = Mathf.Clamp(cameraFOV, maxFOV, minFOV);
         Camera.main.orthographicSize = cameraFOV;
 
@@ -59,6 +73,21 @@ public class Core : MonoBehaviour
     //listening, locating, and sound management functions
     void EarControl()
     {
+        //Move Player position (X and Y axis) to position of mouse click
+        /*
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Left Mouse Button Pressed");
+            clickPosition = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.position = clickPosition;
+            //this should be refactored so that it has to hit the colider of the environment, limiting the click movement only environment
 
+        }
+
+        if ((Vector2)transform.position != clickPosition && Input.GetMouseButton(0))
+        {
+            transform.position = Vector2.MoveTowards(transform.position, clickPosition, moveSpeed * Time.deltaTime);
+        }
+        */
     }
 }
