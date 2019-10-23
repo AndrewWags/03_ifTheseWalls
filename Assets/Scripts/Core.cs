81ec40f18e60b28fc81267fc955fba2954d46528
andrewwags <andrewwags@gmail.com>
2019-10-22 17:01:24 -0700

    Added stand in Environment graphic, started Core Mechanics Prototype

---
+++ Assets/Scripts/Core.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Core : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Camera, motion, navigation controll functions
+    void EyeControl()
+    {
+
+    }
+
+    //selecting, tapping, grabbing functions
+    void HandControl()
+    {
+
+    }
+
+    //listening, locating, and sound management functions
+    void EarControl()
+    {
+
+    }
+}
