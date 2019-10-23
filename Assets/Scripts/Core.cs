67bc50c60a9791014d7774fd13f8933e815e0ea1
andrewwags <andrewwags@gmail.com>
2019-10-22 19:06:21 -0700

    Added player movement with left mouse click to Core Mechanics

--- Assets/Scripts/Core.cs
+++ Assets/Scripts/Core.cs
@@ -4,22 +4,42 @@ using UnityEngine;
 
 public class Core : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private Vector2 clickPosition;
+    public float moveSpeed = 3.0f;
+    
+
+    [SerializeField] Transform target;
+
     void Start()
     {
-        
+        clickPosition = transform.position;
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        EyeControl();
     }
 
-    //Camera, motion, navigation controll functions
+    //Mouse click player motion control
     void EyeControl()
     {
+        //Move Player position (X and Z axis) to position of mouse click
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Left Mouse Button Pressed");
+            clickPosition = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.position = clickPosition;
 
+        }
+        if ((Vector2)transform.position != clickPosition)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, clickPosition, moveSpeed * Time.deltaTime);
+        }
+
+        //Zoom in and out player position with scroll wheel
+        
     }
 
     //selecting, tapping, grabbing functions
