79e65dc09cc66dd64b90c8f6312b29c119773b97
andrewwags <andrewwags@gmail.com>
2019-10-27 05:22:53 -0700

    Added GameManager, basic point and click navigation, Node Classes, Tweener Asset

---
+++ Assets/Scripts/V02/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager ins;
+
+    [HideInInspector]
+    public Node currentNode;
+
+    //very bad singleton, refresh and look up later
+    void Awake()
+    {
+        ins = this;
+    }
+
+    void Update()
+    {
+       if(Input.GetMouseButtonDown(1) && currentNode.GetComponent<Prop>() != null)
+        {
+            currentNode.GetComponent<Prop>().loc.Arrive();
+        } 
+    }
+
+}
