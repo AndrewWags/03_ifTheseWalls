6a43ec074e10b3b0ccee6e443a860dc4dd2d0341
andrewwags <andrewwags@gmail.com>
2019-10-27 05:22:53 -0700

    Added GameManager, basic point and click navigation, Node Classes, Tweener Asset

--- Assets/Scripts/V02/Node.cs
+++ Assets/Scripts/V02/Node.cs
@@ -16,9 +16,52 @@ public abstract class Node : MonoBehaviour
         col = GetComponent<Collider>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMouseDown()
     {
+        Arrive();
+    }
+
+    public void Arrive()
+    {
+        //leave existing currentNode
+        if(GameManager.ins.currentNode != null)
+        {
+            GameManager.ins.currentNode.Leave();
+        }
         
+        //set current node
+        GameManager.ins.currentNode = this;
+
+        //move camera
+        Camera.main.transform.position = cameraPosition.position;
+        Camera.main.transform.rotation = cameraPosition.rotation;
+
+        //turn off own collider
+        if(col != null)
+        {
+            //if collider is enabled, turn it off
+            col.enabled = false;
+        }
+
+        //turn on all reachable nodes colliders
+        foreach(Node node in rechableNodes)
+        {
+            if(node.col != null)
+            {
+                node.col.enabled = true;
+            }
+        }
+    }
+
+    public void Leave()
+    {
+        //turn off all reachable nodes colliders
+        foreach (Node node in rechableNodes)
+        {
+            if (node.col != null)
+            {
+                node.col.enabled = false;
+            }
+        }
     }
 }
