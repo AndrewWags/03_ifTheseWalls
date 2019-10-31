ed2c592876714e178caf517aaad0f1a356b51cf0
Justin Pando <justin.pando@gmail.com>
2019-10-30 18:31:05 -0700

    Progress on dialogue system

---
+++ Assets/Scripts/Encounters/EncounterView.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EncounterView : MonoBehaviour
+{
+    public Image portrait;
+    public Button talkButton;
+    public Button eatButton;
+    public Text textField;
+}
