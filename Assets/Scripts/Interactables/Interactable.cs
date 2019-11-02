f41a87052e6c413f2e19dd13c087c37a0902e531
andrewwags <andrewwags@gmail.com>
2019-11-02 11:55:04 -0700

    Added interactable class - created first interactions with 2 walls

---
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Prop))]
+public class Interactable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.enabled = false;
+    }
+
+    public void Interact()
+    {
+        Debug.Log("interactiong with " + name);
+    }
+}
