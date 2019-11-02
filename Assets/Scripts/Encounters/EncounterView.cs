b8ac4180acbb76c60773c2a7c95a338868c231c1
andrewwags <andrewwags@gmail.com>
2019-11-01 23:24:13 -0700

    Sucessfully imported encounters system into current wroking project files

---
+++ Assets/Scripts/Encounters/EncounterView.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EncounterView : MonoBehaviour
+{
+    public Image portrait;
+    public Button talkButton;
+    public Button eatButton;
+    public Button continueButton;
+    public Text textField;
+    public Text nameTextField;
+}
