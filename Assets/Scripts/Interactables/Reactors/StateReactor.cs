d61f39f59b665fe6ce55324fce4e436ce62b90c0
andrewwags <andrewwags@gmail.com>
2019-11-02 15:22:26 -0700

    Initial development of state/prereq system - Added switcher and debug reactor

---
+++ Assets/Scripts/Interactables/Reactors/StateReactor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Switcher))]
+public abstract class StateReactor : MonoBehaviour
+{
+    protected Switcher switcher;
+
+    protected virtual void Awake()
+    {
+        switcher = GetComponent<Switcher>();
+    
+    }
+
+    public virtual void React()
+    {
+        Debug.Log(name + "'s state is" + switcher.state);
+    }
+}
---
+++ Assets/Scripts/Interactables/Switcher.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switcher : Interactable
+{
+    public bool state;
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        state = !state;
+
+        if(GetComponent<StateReactor>() != null)
+        {
+            GetComponent<StateReactor>().React();
+        }
+
+    }
+}
