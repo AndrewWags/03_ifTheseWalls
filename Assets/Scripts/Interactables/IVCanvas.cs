625cb83fdd6c9fd9a055f09eb6a1b2b6b301f7f3
andrewwags <andrewwags@gmail.com>
2019-11-02 13:58:32 -0700

    Image viewer complete and fully functional

--- Assets/Scripts/Interactables/IVCanvas.cs
+++ Assets/Scripts/Interactables/IVCanvas.cs
@@ -9,7 +9,21 @@ public class IVCanvas : MonoBehaviour
 
     public void Activate(Sprite pic)
     {
+        GameManager.ins.currentNode.SetReachableNodes(false);
+        //if no colider on something, this needs to be amended or it will throw and error looking for one. 
+        GameManager.ins.currentNode.col.enabled = false;
+
         gameObject.SetActive(true);
         imageHolder.sprite = pic;
+    }
+
+    public void Close()
+    {
+        GameManager.ins.currentNode.SetReachableNodes(true);
+        //if no colider on something, this needs to be amended or it will throw and error looking for one. 
+        GameManager.ins.currentNode.col.enabled = true;
+
+        gameObject.SetActive(false);
+        imageHolder.sprite = null;
     }
 }
